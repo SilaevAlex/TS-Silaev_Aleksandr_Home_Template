77bcc0609cf5986490b818ef94560f603223a441
Aleksandr <dp110393sad@gmail.com>
2024-04-22 17:39:21 +0300

    add hw

--- src/les14_HW.ts
+++ src/les14_HW.ts
@@ -1,51 +1,66 @@
-function Memoize() {
-    const cache: Record<string, number> = {};
-    return function <T>(
-        originalMethod: (value: number) => number,
-        context: { kind: string },
+
+    function Memoize<T, A extends any[], R>(
+        originalMethod: (...args: A) => R,
+        context: ClassMethodDecoratorContext<T, (...args: A) => R>
     ) {
-        if (context.kind !== 'method') throw new Error('');
-        function replaceWithValue(this: T, value: number): number {
-            const key = String(value);
-            if (key in cache) {
-                return cache[key];
-            } else {
-                const calculate: number = originalMethod.apply(this, [value]);
-                cache[key] = calculate;
-                return calculate;
+        if (context.kind !== "method") throw new Error("Method-only-decorator");
+
+        const cacheMap = new Map<string, any>();
+
+        function getCacheDataIfContains(this: T, ...args: A): R {
+            const cacheKey = `${args.join('_')}`;
+
+            if (cacheMap.has(cacheKey)) {
+                console.log("cacheMap has already contained value")
+                return cacheMap.get(cacheKey);
             }
+
+            const result = originalMethod.apply(this, args);
+            cacheMap.set(cacheKey, result);
+
+            return result;
         }
-        return replaceWithValue;
-    };
-}
 
-class Calculations {
-    @Memoize()
-    double(value: number): number {
-        return value * 2;
+        return getCacheDataIfContains;
     }
-}
 
-function Debounce(delay: number = 0) {
-    let timer: ReturnType<typeof setTimeout>;
-    return function <T>(
-        originalMethod: (...args: any[]) => void,
-        context: { kind: string },
-    ): (...args: any[]) => void {
-        if (context.kind !== 'method') throw new Error('');
-        return function(this: T, ...args: any[]): void {
-            clearTimeout(timer);
-            timer = setTimeout(() => originalMethod.apply(this, args), delay);
-        };
-    };
-}
-
-class DebouncedActions {
-    @Debounce(5000)
-    executeAfterDelay(): void {
-        console.log('Debounced');
+    class Calculations {
+        @Memoize
+        showLog(key: string, value: string) {
+            return `${key}: ${value}`;
+        }
     }
-}
 
-const actions = new DebouncedActions();
-actions.executeAfterDelay();
+    const str = new Calculations();
+    console.log(str.showLog("key1", "value1"));
+
+
+    function Debounce<T, A extends any[], R>(
+        originalMethod: (...args: A) => R,
+        context: ClassMethodDecoratorContext<T, (...args: A) => R>
+    ) {
+        if (context.kind !== "method") throw new Error("Debounce can only be applied to methods");
+        let timeout: NodeJS.Timeout;
+
+        function getDelay(this: T, ...args: A): R {
+            clearTimeout(timeout);
+
+            timeout = setTimeout(() => {
+                originalMethod.apply(this, args);
+            }, args[0]);
+
+            return undefined as any;
+        }
+
+        return getDelay;
+    }
+
+    class Example {
+        @Debounce
+        delay(ms: number) {
+            console.log(`Delayed action executed after ${ms} ms`);
+        }
+    }
+
+    const instance = new Example();
+    instance.delay(3000);
